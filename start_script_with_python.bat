7ce4d8b5945152f9f7a9effff18ecec48976c5d6
insane <info@insane.software>
2023-08-26 20:59:46 +0200

    fixed bat file

--- start_script_with_python.bat
+++ start_script_with_python.bat
@@ -3,7 +3,7 @@ echo off
 cls
 echo Installing Dependencies:
 echo .
-pip install opencv-python PyGetWindow PyAutoGUI pywin32 tkinter
+pip install opencv-python PyGetWindow PyAutoGUI pywin32
 echo .
 echo Done! installing dependencies. Starting script!
 echo .
