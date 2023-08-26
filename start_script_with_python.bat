92c8fea52b75bfc4768e8621dcc87f7e0f5af1c6
insane <info@insane.software>
2023-08-26 18:39:49 +0200

    Added a start.bat for python fiorst time users!

---
+++ start_script_with_python.bat
@@ -0,0 +1,10 @@
+color 0A
+echo off
+cls
+echo Installing Dependencies:
+echo .
+pip install opencv-python PyGetWindow PyAutoGUI pywin32
+echo .
+echo Done! installing dependencies. Starting script!
+echo .
+python InsaneChestDeleter.py
